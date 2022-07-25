2ee49e8f1dec0f3c04b1022e821fe703522078ff
Volkov Alexander <volkov.525@gmail.com>
2022-07-25 16:25:35 +0300

    finish layout

--- src/pages/Card.vue
+++ src/pages/Card.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
 const tab1 = ref('cod')
-const splitterModel = ref(50)
-
-const hei = computed(() => {
-	return 'height: ' + (window.innerHeight - 180) + 'px;'
-})
+const splitterModel = ref(40)
 </script>
 
 <template lang="pug">
@@ -16,7 +12,7 @@ q-page
 			q-icon(name="mdi-book-open-page-variant-outline").q-mr-sm
 			|Коды полномочий
 
-		q-splitter(v-model="splitterModel" :style="hei")
+		q-splitter(v-model="splitterModel" )
 			template(v-slot:before)
 				q-card.left
 					p laksjl
@@ -29,25 +25,36 @@ q-page
 					p laksjl
 			template(v-slot:after)
 				.q-pl-sm
-					q-tabs(v-model="tab1" align="left" dense)
+					q-tabs(v-model="tab1" align="left" dense active-color="primary" :stretch="false")
 						q-tab(name="cod" label="Сведения о коде")
 						q-tab(name="sprav" label="Сведения о справочнике")
-						q-tab()
+					q-card.right
+						q-tab-panels(v-model="tab1")
+							q-tab-panel(name="cod")
+								p cod
+							q-tab-panel(name="sprav")
+								p sprav
 
 </template>
 
 <style scoped lang="scss">
-//@import '@/assets/css/colors.scss';
 .center {
 	height: calc(100vh - 220px);
 	display: flex;
 	justify-content: center;
 	align-items: center;
 }
+.left,
+.right {
+	box-shadow: none;
+	border-radius: 4px;
+}
 .left {
 	box-shadow: none;
-	border-radius: 0.5rem;
+	border-radius: 4px;
 	margin-right: 0.5rem;
-	margin-top: 2rem;
+	margin-top: 34px;
+	padding: 1rem;
+	height: calc(100vh - 210px);
 }
 </style>
