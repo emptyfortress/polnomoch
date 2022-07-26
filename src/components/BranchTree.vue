9244ae42a0c2975dc5f41d6470ac0f6f4804a112
Volkov Alexander <volkov.525@gmail.com>
2022-07-26 10:03:26 +0300

    add scrollarea

--- src/components/BranchTree.vue
+++ src/components/BranchTree.vue
@@ -1,54 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import type { Ref } from 'vue'
 import SvgIcon from '@/components/SvgIcon.vue'
 import WordHighlighter from 'vue-word-highlighter'
+import { useInfo } from '@/stores/info'
+
+const myinfo = useInfo()
 
 const selected = ref(['0'])
-const expanded = ref(['0', '1'])
+const expanded = ref(['0', '4'])
 const filter = ref('')
-const nodes = [
-	{
-		id: '0',
-		label: 'Справочники кодов полномочий',
-		header: 'root',
-		children: [
-			{
-				id: '1',
-				icon: 'node-folder',
-				label: 'ФНС (федеральная налоговая служба)',
-				children: [
-					{
-						id: '5',
-						icon: 'spravochnik',
-						label: 'Fuck',
-						children: [],
-						body: 'story',
-						story: 'this is story',
-					},
-				],
-			},
-			{
-				id: '2',
-				icon: 'node-folder',
-				label: 'ФК (федеральное казначейство)',
-				children: [],
-			},
-			{
-				id: '3',
-				icon: 'node-folder',
-				label: 'МЦР (министерство цифрового развития)',
-				children: [],
-			},
-			{
-				id: '4',
-				icon: 'node-folder',
-				label: 'ООО "ДоксВижн"',
-				children: [],
-			},
-		],
-	},
-]
 </script>
 
 <template lang="pug">
@@ -58,26 +18,24 @@ const nodes = [
 		template(v-slot:prepend)
 			q-icon(name="mdi-magnify")
 
-q-tree(:nodes="nodes"
-	node-key="id"
-	no-results-label="Ничего нет"
-	no-selection-unset
-	v-model:selected="selected"
-	v-model:expanded="expanded"
-	:filter="filter")
+q-scroll-area.scroll
+	q-tree(:nodes="myinfo.nodes"
+		node-key="id"
+		no-results-label="Ничего нет"
+		no-selection-unset
+		v-model:selected="selected"
+		v-model:expanded="expanded"
+		:filter="filter")
 
-	template(v-slot:header-root="prop")
-		div(class="row items-center")
-			q-icon(name="mdi-bookshelf" size="md").q-mr-sm
-			div {{ prop.node.label }}
+		template(v-slot:header-root="prop")
+			div(class="row items-center")
+				q-icon(name="mdi-bookshelf" size="md").q-mr-sm
+				div {{ prop.node.label }}
 
-	template(v-slot:default-header="prop")
-		div(class="row items-center")
-			component(:is="SvgIcon" :name="prop.node.icon").ico
-			WordHighlighter(:query="filter").ellipsis {{ prop.node.label }}
-
-	template(v-slot:body-story="prop")
-		span {{ prop.node.story }}
+		template(v-slot:default-header="prop")
+			.item
+				component(:is="SvgIcon" :name="prop.node.icon").ico
+				WordHighlighter(:query="filter").ellipsis {{ prop.node.label }}
 
 </template>
 
@@ -86,8 +44,18 @@ q-tree(:nodes="nodes"
 .input {
 	width: 200px;
 }
+.item {
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	overflow: auto;
+}
 .ico {
 	font-size: 0.8rem;
 	margin-right: 0.5rem;
+	flex-shrink: 0;
+}
+.scroll {
+	height: calc(100% - 20px);
 }
 </style>
--- src/stores/info.js
+++ src/stores/info.js
@@ -1,9 +1,130 @@
 import { defineStore } from 'pinia'
+import { uid } from 'quasar'
 
 export const useInfo = defineStore({
 	id: 'info',
 	state: () => ({
-		infoDrawer: false,
+		nodes: [
+			{
+				id: '0',
+				label: 'Справочники кодов полномочий',
+				header: 'root',
+				children: [
+					{
+						id: uid(),
+						icon: 'node-folder',
+						label: 'ФНС (федеральная налоговая служба)',
+						children: [
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_11 отклонение представляет собой' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_12 вектор угловой скорости' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_13 явно видно по фазовой траектории' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_14 время набора максимальной скорости',
+							},
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_15 согласно третьему закону Ньютона' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_16 преобразует периодический уход гироскопа',
+							},
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_17 при малых значениях коэффициента податливости',
+							},
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_18 с постоянным ускорением' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_19 позволяет исключить из рассмотрения',
+							},
+						],
+					},
+					{
+						id: '2',
+						icon: 'node-folder',
+						label: 'ФК (федеральное казначейство)',
+						children: [
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_21 отклонение представляет собой' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_22 вектор угловой скорости' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_23 явно видно по фазовой траектории' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_24 время набора максимальной скорости',
+							},
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_25 согласно третьему закону Ньютона' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_26 преобразует периодический уход гироскопа',
+							},
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_27 при малых значениях коэффициента податливости',
+							},
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_28 с постоянным ускорением' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_29 позволяет исключить из рассмотрения',
+							},
+						],
+					},
+					{
+						id: '3',
+						icon: 'node-folder',
+						label: 'МЦР (министерство цифрового развития)',
+						children: [
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_31 отклонение представляет собой' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_32 вектор угловой скорости' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_33 явно видно по фазовой траектории' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_34 время набора максимальной скорости',
+							},
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_35 согласно третьему закону Ньютона' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_36 преобразует периодический уход гироскопа',
+							},
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_37 при малых значениях коэффициента податливости',
+							},
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_38 с постоянным ускорением' },
+							{
+								id: uid(),
+								icon: 'spravochnik',
+								label: 'CPDV_39 позволяет исключить из рассмотрения',
+							},
+						],
+					},
+					{
+						id: '4',
+						icon: 'node-folder',
+						label: 'ООО "ДоксВижн"',
+						children: [
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_1 договоры до 50 т. руб.' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_2 договоры до 100 т. руб.' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_3 договоры от 500 т. руб.' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_4 договоры от 1 млн. руб.' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_5 договоры с зарубежными партнерами' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_6 приказы по группе компаний' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_7 приказы по филиалу' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_8 больничные листы' },
+							{ id: uid(), icon: 'spravochnik', label: 'CPDV_9 служебные записки' },
+						],
+					},
+				],
+			},
+		],
 	}),
 	getters: {},
 	actions: {},
